0d034e21f1d7a670e1977b7ca5556abe718ce96b
dilrajsooch <65379380+dilrajsooch@users.noreply.github.com>
2024-04-02 22:00:38 -0400

    Update main.c

--- main.c
+++ main.c
@@ -2,158 +2,125 @@
 #include <stdlib.h>
 #include <time.h>
 
-// Enum for factions
-enum Faction {
-    RED,
-    BLUE,
-    GREEN
-};
+//Dilraj Sooch Assignment 3
+/*Starting from an empty project, you must:
 
-struct UserInfo {       //struct for userinfo
-    char username[9];
-    int level;
-    enum Faction faction;
-};
+Create two arrays of length 20,000 and insert the same random number into each index (i.e. the two arrays should be separate arrays but contain identical values).
+Sort the first array using a bubble sort. Print the length of time it took to perform the sort.
+Sort the second array using a heap sort. Print the length of time it took to perform the heap sort.
+Print the time it took to perform each sort.
+Insert a break point at the end of your code and during the presentation show the contents of the two arrays after they have been sorted using the debugger.
+Note: This assignment depends on the speed of your computer's processor. If you find 20,000 is taking too long, you may shorten it. If it happens too quickly and you don't see a difference between the two runs, you may increase the length of the array. If you encounter a warning in Visual Studio about the array length, you can ignore it.
 
-struct QNode {          //linked list node
-    struct UserInfo userinfo;
-    struct QNode* next;
-};
+For the timing, you can use the following algorithm (or any timing code you wish):
 
-struct Queue {              //queue struct
-    struct QNode* front, *rear;
-};
+#include <time.h>
 
-char generateRandomChar() {                 //random char function for username
-    char charset[] = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-    int charsetSize = sizeof(charset) - 1;
-    return charset[rand() % charsetSize];               //using charset as an array and the rand value is choosing the index based on the parameters of the size
-}
+…
 
-void generateRandomUsername(char* username, int length) {       //for loop for 10 characters but coded it as it could be any length for futire purposes
-    for (int i = 0; i < length - 1; ++i) {
-        username[i] = generateRandomChar();
-    }
-    username[length - 1] = '\0'; // Null-terminate the string
-}
-
-struct QNode* createNewNode() {
-    struct QNode* newNode = (struct QNode*)malloc(sizeof(struct QNode));    //allocate memory
-    generateRandomUsername(newNode->userinfo.username, 11);                 //username generator from above functions
-    newNode->userinfo.level = rand() % 60 + 1;                              //1-60
-
-    //Randomly choose faction (red, blue, or green)
-    enum Faction factionChoice = rand() % 3;                //faction can be 0,1,2
-    newNode->userinfo.faction = factionChoice;              //red blue or green
-
-    newNode->next = NULL;           //pointing at next node //pusing to rear
-    return newNode;
-}
-
-//init emptyqueue
-struct Queue* createQueue() {
-    struct Queue* q = (struct Queue*)malloc(sizeof(struct Queue));
-    q->front = NULL, q->rear = NULL;
-    return q;
-}
-
-//adding a node to end of queue
-void enqueue(struct Queue* q) {
-    struct QNode* temp = createNewNode();
-    if (q->rear == NULL) {              //if rear is emppty node will be front and rear
-        q->front = q->rear = temp;
-    } else {                    //moving rear pointer to the newly added node
-        q->rear->next = temp;
-        q->rear = temp;
-    }
-}
-
-//must dequeue the front node and move front node to next and print to dequeued node to system
-void dequeue(struct Queue* q) {
-    if (q->front == NULL)
-        return;                 //nothing in queue
-
-    struct QNode* temp = q->front;      //temp ptr to placehold for front while front ptr moves to the next node
-    q->front = q->front->next;
-
-    if (q->front == NULL) {
-        q->rear = NULL;             //nothing in queue
-    }
-
-    char* factionStr;                   //variable for string colour
-    switch (temp->userinfo.faction) {
-        case RED:                       //enum to string
-            factionStr = "RED";
-            break;
-        case BLUE:
-            factionStr = "BLUE";
-            break;
-        case GREEN:
-            factionStr = "GREEN";
-            break;
-    }
-
-    printf("The Dequeued Node from Queue is: Username: %s, Level: %d, Faction: %s\n",
-           temp->userinfo.username, temp->userinfo.level, factionStr);
-
-    free(temp);
-}
+clock_t startTime = clock();
 
 
-void printQueue(struct Queue* p) {
-    if (p->front == NULL) {
-        printf("Queue is empty.\n");            //empty check
-        return;
-    }
-    //printing queue function
 
-    struct QNode* current = p->front;
+//Sort happens here
 
-    while (current != NULL) {
-        char* factionStr;
-        switch (current->userinfo.faction) {
-            case RED:
-                factionStr = "RED";             //enum to string
-                break;
-            case BLUE:
-                factionStr = "BLUE";
-                break;
-            case GREEN:
-                factionStr = "GREEN";
-                break;
 
+
+clock_t endTime = clock();
+
+double sortTime = (double)(endTime - startTime) / CLOCKS_PER_SEC;
+ */
+
+// Bubble Sort
+void bubbleSort(int arr[], int n) {
+    int i, j, temp;
+    for (i = 0; i < n-1; i++) {
+        for (j = 0; j < n-i-1; j++) {
+            if (arr[j] > arr[j+1]) {
+                // Swap elements
+                temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
         }
-
-        printf("Username: %s, Level: %d, Faction: %s\n",
-               current->userinfo.username, current->userinfo.level, factionStr);
-        current = current->next;
     }
 }
+
+// heap sort a subtree rooted with node i which is an index in arr[]
+void heapify(int arr[], int n, int i) {
+    int largest = i; // init largest as root
+    int left = 2*i + 1;
+    int right = 2*i + 2;
+
+    // if left child is larger than root
+    if (left < n && arr[left] > arr[largest])
+        largest = left;
+
+    // if right child is larger than largest
+    if (right < n && arr[right] > arr[largest])
+        largest = right;
+
+    // iff largest is not root
+    if (largest != i) {
+        // swap arr[i] and arr[largest] using temp
+        int temp = arr[i];
+        arr[i] = arr[largest];
+        arr[largest] = temp;
+
+        // Recursively heapsort the affected sub-tree
+        heapify(arr, n, largest);
+    }
+}
+
+// printing function for array
+void printArray(int arr[], int n) {
+    for (int i = 0; i < n; ++i) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
 
 int main() {
-    srand(time(NULL));          //seed
-    // Create a queue
-    struct Queue* queue = createQueue();
+    const int size = 20000;
+    int arr_bubble[size], arr_heap[size];
+    clock_t startTime, endTime;
+    double sortTime;
 
-    // Hardcoded number of users
-    int numUsers = 3;           //control for number of users generated
-
-    // Enqueue users using a for loop
-    for (int i = 0; i < numUsers; ++i) {
-        enqueue(queue);
+    // Generating random numbers and populating arrays using seed
+    srand(time(NULL));
+    for (int i = 0; i < size; i++) {
+        int random_num = rand() % 1000; // Random number between 0 and 999
+        arr_bubble[i] = random_num; //putting same random number into each index
+        arr_heap[i] = random_num;
     }
 
-    // Print the initial state of the queue
-    printf("Initial Queue:\n\n");
-    printQueue(queue);
+    //sorting array using bubble sort
+    startTime = clock();
+    bubbleSort(arr_bubble, size);
+    endTime = clock();
+    sortTime = (double)(endTime - startTime) / CLOCKS_PER_SEC;
+    printf("Time taken for bubble sort: %lf seconds\n", sortTime);
+    printf("Bubble sorted array: ");
+    printArray(arr_bubble, size);
 
-    // Dequeue and print each user until the queue is empty
-    while (queue->front != NULL) {
-        printf("\nAfter Dequeue:\n\n");
-        dequeue(queue);
-        printf("\nPresent Queue:\n");
-        printQueue(queue);
+    //sorting array using heap sort
+    startTime = clock();
+    for (int i = size / 2 - 1; i >= 0; i--)
+        heapify(arr_heap, size, i);
+    for (int i = size - 1; i >= 0; i--) {
+        int temp = arr_heap[0];
+        arr_heap[0] = arr_heap[i];
+        arr_heap[i] = temp;
+        heapify(arr_heap, i, 0);
     }
+    endTime = clock();
+    sortTime = (double)(endTime - startTime) / CLOCKS_PER_SEC;
+    printf("Time taken for heap sort: %lf seconds\n", sortTime);
+    printf("Heap sorted array: ");
+    printArray(arr_heap, size);
+
+
 
     return 0;
 }
